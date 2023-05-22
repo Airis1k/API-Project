cda4da8b2ffc55601ec34da82c240ab21f0396b4
Airis1k <airingasstar@gmail.com>
2023-05-22 10:37:42 +0300

    added users

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-hello();
+init();
 
-function hello() {
- console.log("hello world!");
+function init() {
+   // get all the users
+   getUsers();
+}
+
+async function getUsers() {
+   // fetch users API
+   const response = await fetch("https://jsonplaceholder.typicode.com/users?_embed=posts");
+   const data = await response.json();
+
+   // Select ul element
+   const ulElement = document.getElementById("user-names");
+
+   data.forEach(element => {
+      // Create li element
+      const liElement = document.createElement("li");
+      // Create a element
+      const aElement = document.createElement("a");
+      // Prints user name and posts count
+      aElement.innerHTML = `${element.name} - <i>(${element.posts.length})</i>`;
+      aElement.href = "/user.html";
+
+      // Insert a element to li
+      liElement.append(aElement);
+      // Insert li element to ul
+      ulElement.append(liElement);
+   });
 }
