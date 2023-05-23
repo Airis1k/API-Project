45e74e4acfb9c5832a9b2eb6233500dcea137c8e
Airis1k <airingasstar@gmail.com>
2023-05-23 18:17:39 +0300

    updated navigation classes, added post.html file

--- navigation.js
+++ navigation.js
@@ -1,22 +1,51 @@
 navigationBetweenWindows();
 
 function navigationBetweenWindows() {
+   const headerElement = document.createElement("header");
+   const navElement = document.createElement("nav");
    const ulElement = document.createElement("ul");
+   ulElement.classList.add("window-elements");
 
-   const home = createNavigation("HOME", "/");
-   const users = createNavigation("USERS", "/users.html");
-   const posts = createNavigation("POSTS", "/posts.html");
-   const albums = createNavigation("ALBUMS", "/albums.html");
+   const homeObj = {"name":"HOME","link":"/"};
+   const usersObj = {"name":"USERS","link":"/users.html"};
+   const postsObj = {"name":"POSTS","link":"/posts.html"};
+   const albumsObj = {"name":"ALBUMS","link":"/albums.html"};
 
+   const home = createNavigation(homeObj.name, homeObj.link);
+   const users = createNavigation(usersObj.name, usersObj.link);
+   const posts = createNavigation(postsObj.name, postsObj.link);
+   const albums = createNavigation(albumsObj.name, albumsObj.link);
+   
+   
+   headerElement.append(navElement);
+   navElement.append(ulElement);
    ulElement.append(home, users, posts, albums);
-   document.body.prepend(ulElement);
+   document.body.prepend(headerElement);
+
+   // Active window
+   const windowLinks = [homeObj, usersObj, postsObj, albumsObj];
+   activeWindow(windowLinks);
+}
+
+function activeWindow(windowNames) {
+   const activeWindow = window.document.location.pathname;
+   const aElements = document.querySelectorAll("a.window-link");
+
+   windowNames.forEach(element => {
+      if (activeWindow === element.link) {
+         
+         aElements.forEach(elem2 => {
+            if (element.name === elem2.textContent) {
+               elem2.classList.add("active-link");
+            }
+         });
+      }
+   });
 }
 
 function createNavigation(name, link) {
    const navigation = document.createElement("li");
-   navigation.innerHTML = `<a href="${link}">${name}</a>`;
+   navigation.innerHTML = `<a href="${link}" class="window-link">${name}</a>`;
 
    return navigation;
 }
-
-// TODO: Pasirinkta nuoroda turi buti paryskinta; Pataisyti navigacijos stiliu
