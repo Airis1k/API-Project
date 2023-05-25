9c523c498a8d494f88a05e5540762e31c65c45d1
Airis1k <airingasstar@gmail.com>
2023-05-25 12:06:33 +0300

    added lightbox

--- album-script.js
+++ album-script.js
@@ -11,10 +11,17 @@ async function getUserAllAlbums() {
    const divElement = document.createElement("div");
    // div elem for photos
    const photosDiv = document.createElement("div");
+   photosDiv.classList.add("gallery");
 
-   const albumId = 1;
+   // returns URL parameters
+   const queryParams = window.document.location.search;
+   const urlParams = new URLSearchParams(queryParams);
+   const albumId = urlParams.get("album_id");
+
    // fetch albums API
-   const response = await fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}?_expand=user&_embed=photos`);
+   const response = await fetch(
+      `https://jsonplaceholder.typicode.com/albums/${albumId}?_expand=user&_embed=photos`
+   );
    const data = await response.json();
 
    // h2 elem to display album title
@@ -23,10 +30,24 @@ async function getUserAllAlbums() {
    // div elem to display album author
    const albumAuthor = document.createElement("div");
    albumAuthor.classList.add("album-author");
-   albumAuthor.innerHTML = `<b>Author:</b> ${data.user.name}`;
+   albumAuthor.innerHTML = `<b>Author:</b> <a href="/user.html?user_id=${data.userId}">${data.user.name}</a>`;
 
+   // Working with album photos
+   data.photos.forEach((element) => {
+      // a->img elem for photo
+      const aElement = document.createElement("a");
+      aElement.innerHTML = `<img src="${element.thumbnailUrl}" alt="color block">`;
+      aElement.href = element.url;
+
+      // set attribute for aElement
+      const attribute = document.createAttribute("data-lightbox");
+      attribute.value = "mygallery";
+      aElement.setAttributeNode(attribute);
+
+      // insert photo to div elem
+      photosDiv.append(aElement);
+   });
 
    divElement.append(h2Element, albumAuthor, photosDiv);
    mainDiv.append(divElement);
-   console.log(data);
 }
--- albums-script.js
+++ albums-script.js
@@ -72,7 +72,7 @@ async function getUserAlbums() {
          if (elem2.user.name === element) {
             // li elem for every album title
             const liElement = document.createElement("li");
-            liElement.innerHTML = `<a href="/album.html">${elem2.title} <i>(${elem2.photos.length})</i></a>`;
+            liElement.innerHTML = `<a href="/album.html?album_id=${elem2.id}">${elem2.title} <i>(${elem2.photos.length})</i></a>`;
    
             // add li elem to ul
             ulElement.append(liElement);
